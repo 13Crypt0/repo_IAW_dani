99e6458d2916a17dddaae451712e812c3a6c901d
danii <danielmartinezcolomerr@gmail.com>
2025-01-15 13:53:01 +0100

    Acabar sesiones

---
+++ PHP_Ejercicios/EJERCICIOS PHP PARTE 4/sesiones/ejercicio2/ejercicio2_formulario.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * Sesiones (1) 02 - sesiones-1-02-1.php
+ *
+ * @author Escriba aquí su nombre
+ *
+ */
+
+session_name("ejercicio2");
+session_start();
+
+?>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+  <meta charset="utf-8">
+  <title>
+    Formulario Texto 2 (Formulario).
+    Sesiones (1). Sesiones.
+    Escriba aquí su nombre
+  </title>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="../mclibre-php-ejercicios.css" title="Color">
+</head>
+
+<body>
+  <h1>Formulario Texto 2 (Formulario)</h1>
+
+<?php
+
+if (isset($_SESSION["texto"])) {
+    echo "<p>El texto es: <b>$_SESSION[texto]</b></p>\n";
+}
+?>
+
+  <form action="ejercicio2_recogida.php" method="get">
+    <p>
+      <label>
+        Escriba texto:
+        <input type="text" name="texto" size="20" maxlength="20">
+      </label>
+    </p>
+
+    <p>
+      <input type="submit" value="Guardar">
+      <input type="reset">
+    </p>
+  </form>
+
+  <footer>
+    <p>Daniel Martinez Colomer</p>
+  </footer>
+</body>
+</html>
